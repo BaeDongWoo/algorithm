6454227b9afd377a41f57b23abe3c87a0a4fee48
dongwoo <114900672+BaeDongWoo@users.noreply.github.com>
2024-02-27 13:53:33 +0900

    [level 1] Title: 실패율, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,17 +1,14 @@
 function solution(N, stages) {
     var answer = [];
-    let gamer = stages.length;
-    let people = new Array(N+1);
-    for (let i=1;i<=N ;i++){    
-        people[i]=stages.filter((e)=>e===i).length; 
+    let gamer = stages.length; // 총 게이머의 수 
+    let failureRate = []; // 실패율을 저장할 리스트
+    for (let i = 1;i<=N;i++){
+        const stagePeople = stages.filter((e)=>e===i).length; // 스테이지별 인원수
+        const rate = [stagePeople/gamer,i]; // 실패율과 , 스테이지를 저장
+        failureRate.push(rate); 
+        gamer-=stagePeople;// 다음 스테이지의 게이머 수 
     }
-    let failureRate = [];
-    for (let i = 1;i<people.length;i++){
-        const rate = [people[i]/gamer,i];
-        failureRate.push(rate);
-        gamer-=people[i];
-    }
-    failureRate.sort((a,b)=>{
+    failureRate.sort((a,b)=>{// 실패율과 스테이지에 따라 정렬
         if(a[0]===b[0]){
             return a[1]-b[1];
         }
@@ -19,7 +16,7 @@ function solution(N, stages) {
             return b[0]-a[0];
         }
     });
-    failureRate.map((arr)=> answer.push(arr[1]));
+    failureRate.map((arr)=> answer.push(arr[1]));// 스테이지만 출력
     return answer;
 }
 
@@ -28,4 +25,3 @@ function solution(N, stages) {
 // 스테이지에 도달한 플레이어 수 = 현재 스테이지 + 이후 스테이지에 있는 사람들
 
 // 각 스테이지별 머무르고 있는사람들 인원 저장
-// 
