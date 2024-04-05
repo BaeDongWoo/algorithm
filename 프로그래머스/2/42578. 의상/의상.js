7507411f778768f24b4d0a04957b64588af6d2bc
dongwoo <114900672+BaeDongWoo@users.noreply.github.com>
2024-04-05 21:01:05 +0900

    [level 2] Title: 의상, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -0,0 +1,10 @@
+function solution(clothes) {
+  let types = {};
+  let result = 1;
+
+  clothes.forEach((v) => (types[v[1]] = (types[v[1]] || 0) + 1));
+  for (const key in types) {
+    result *= types[key] + 1;
+  }
+  return result - 1;
+}
