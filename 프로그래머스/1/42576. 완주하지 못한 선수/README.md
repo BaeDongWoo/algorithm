954d43b00903aee9817634d0cbb4cf4c5a92a611
dongwoo <114900672+BaeDongWoo@users.noreply.github.com>
2024-07-25 19:12:05 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 136.80 ms, Memory: 59.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 58.5 MB, 시간: 113.42 ms
+메모리: 59.3 MB, 시간: 136.80 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 1일 22:23:26
+2024년 07월 25일 19:12:03
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -2,14 +2,8 @@ function solution(participant, completion) {
     var answer = '';
     participant.sort();
     completion.sort();
-    const findName=()=>{
-        for(let i=0;i<participant.length;i++){
-            if(participant[i]!==completion[i]) {
-                 return participant[i];
-            }
-        }
-       return participant[participant.length-1]
+    for(let i = 0;i<participant.length;i++){
+        if(participant[i]!==completion[i]) return participant[i];
     }
-    answer=findName();
-    return answer;
+    return participant[participant.length-1];
 }
