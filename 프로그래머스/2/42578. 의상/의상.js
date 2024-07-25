6ace63be72d4a3e51acef80d178496407d9d52b9
dongwoo <114900672+BaeDongWoo@users.noreply.github.com>
2024-07-25 20:36:44 +0900

    [level 2] Title: 의상, Time: 3.63 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,10 +1,15 @@
 function solution(clothes) {
-  let types = {};
-  let result = 1;
-
-  clothes.forEach((v) => (types[v[1]] = (types[v[1]] || 0) + 1));
-  for (const key in types) {
-    result *= types[key] + 1;
-  }
-  return result - 1;
+    let result = 1;
+    let clothesTable = new Map();
+    clothes.map((c)=>{
+        if(clothesTable.get(c[1])===undefined) clothesTable.set(c[1],1);
+        else clothesTable.set(c[1],clothesTable.get(c[1])+1);
+    });
+    clothesTable.forEach((c)=>{
+        result*=c+1;
+    })
+    console.log(result);
+    return result-1;
 }
+// 경우의 수는 모든 옷 종류 + 옷 조합
+// 옷 조합 = 모든 옷 곱하기
